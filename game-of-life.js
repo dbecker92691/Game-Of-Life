270eeb582fc25c6bf13854bbab149a73eb6b3cac
dbecker92691 <dbecker92691@gmail.com>
2019-05-28 10:35:00 -0600

    instructions and read.me

--- game-of-life.js
+++ game-of-life.js
@@ -1,6 +1,4 @@
-# Rasa-Game-Of-Life
-
-
+/*
 According to the Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
 Given a board with m by n cells, each cell has an initial state live (1) or dead (0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules:
 * Any live cell with fewer than two live neighbors dies, as if caused by under-population.
@@ -8,7 +6,7 @@ Given a board with m by n cells, each cell has an initial state live (1) or dead
 * Any live cell with more than three live neighbors dies, as if by over-population..
 * Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
 * Write a function to compute the next state (after one update) of the board given its current state.
-
+*/
 // Example:
 nextGeneration([
   [1, 1, 1],
